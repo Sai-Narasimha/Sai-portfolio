5b223342b52a5d68aa7770b143e9ccc8f388bf95
Sai-Narasimha <bukkapatnam.sainarasimha@gmail.com>
2022-08-23 18:01:08 +0530

    resume changed

--- scriptAndPages/about.js
+++ scriptAndPages/about.js
@@ -1,4 +1,4 @@
-let about = document.getElementById("about")
+let about = document.getElementById("about");
 about.innerHTML = `
 <section class="about" id="about">
         <div class="max-width">
@@ -35,7 +35,7 @@ about.innerHTML = `
                     <a  href="tel:+91 8074352708">
                         <p class="iconsAboutSection"> <i class="fas fa-phone"></i> +91-8074352708</p>
                     </a>
-                    <a href="https://drive.google.com/file/d/1zyAcvxdSd2W3wdpq4HN44GYCpOr-WHDZ/view?usp=sharing"
+                    <a href="https://drive.google.com/file/d/1IiRPI9Q0p6131ZmCYD6iLgDOoGfNvtAc/view?usp=sharing"
                         target="_blank" class="resumeToDrive">Resume</a>
                 </div>
             </div>
